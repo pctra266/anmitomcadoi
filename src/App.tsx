9fc23bf820459d52c09fc5998ec4da2a1ecdbfd8
Tra Pham <phamtra001@gmail.com>
2026-01-07 21:19:53 +0700

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Wheel from './Components/Wheel/Wheel'
+import DailyGoal from './Components/DailyGoal/DailyGoal' // Nhớ import component mới
 
 function App() {
 
   return (
-    <>
-    <Wheel></Wheel>
-    </>
+    <BrowserRouter>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Wheel Game</Link>
+          </li>
+          <li>
+            <Link to="/daily-goal">Daily Goal</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Wheel />} />
+          <Route path="/daily-goal" element={<DailyGoal />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Components/DailyGoal/DailyGoal.tsx
@@ -0,0 +1,14 @@
+// Components/DailyGoal/DailyGoal.jsx
+import React from 'react';
+
+const DailyGoal = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Daily Goal</h1>
+      <p>Đây là trang mục tiêu hàng ngày.</p>
+      {/* Nội dung của component DailyGoal sẽ ở đây */}
+    </div>
+  );
+};
+
+export default DailyGoal;
